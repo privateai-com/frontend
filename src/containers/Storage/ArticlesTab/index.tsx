1a576877606cf2e117e24d2f0b787c1a488f6012
IntegralTeam <repo@sfxdx.com>
2023-09-27 11:49:50 +0700

    add articles mobile tab

--- src/containers/Storage/ArticlesTab/index.tsx
+++ src/containers/Storage/ArticlesTab/index.tsx
@@ -8,12 +8,13 @@ import {
   TextInput,
 } from 'components';
 
-import { routes } from 'appConstants';
-import { usePageCount } from 'hooks';
+import { ScreenWidth, routes } from 'appConstants';
+import { usePageCount, useScreenWidth } from 'hooks';
 import styles from './styles.module.scss';
 
 import { content, initialObj } from './data';
 import { useColumns } from './columns';
+import { Article } from './Article';
 
 const itemsOnPageQuantity = 6;
 
@@ -21,6 +22,7 @@ export const ArticlesTab = () => {
   const [search, setSearch] = useState('');
   const columns = useColumns();
   const [currentPageIndex, setCurrentPageIndex] = useState(0);
+  const isMobile = useScreenWidth(ScreenWidth.mobile);
   
   const data = useMemo(() => {
     if (content.length >= 6) {
@@ -56,16 +58,34 @@ export const ArticlesTab = () => {
         </Button>
       </div>
 
-      <Table
-        columns={columns as unknown as Column<object>[]}
-        data={tableData}
-        className={styles.table}
-      />
-      <Pagination
-        onChange={setCurrentPageIndex}
-        pageCount={pageCount}
-        className={styles.pagination}
-      />
+      {isMobile ? (
+        <div className={styles.table_mobile}>
+          {content.map(({
+            id, name, status, core,
+          }) => (
+            <Article
+              key={id}
+              name={name}
+              status={status}
+              core={core}
+            />
+          ))}
+        </div>
+      ) : (
+        <>
+          <Table
+            columns={columns as unknown as Column<object>[]}
+            data={tableData}
+            className={styles.table}
+          />
+          <Pagination
+            onChange={setCurrentPageIndex}
+            pageCount={pageCount}
+            className={styles.pagination}
+          />
+        </>
+      )}
+      
     </>
   );
 };
