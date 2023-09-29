7c74578ec9c5b8a0601bfaaadbe8d48f4568ffb9
IntegralTeam <repo@sfxdx.com>
2023-09-29 12:04:07 +0300

    add Requester to requests page

--- src/containers/Requests/RequestTab/columns.tsx
+++ src/containers/Requests/RequestTab/columns.tsx
@@ -1,11 +1,44 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 import Link from 'next/link';
-import { Button } from 'components';
+import { Button, Requester } from 'components';
 import { ItemRowProps } from 'types';
 import { routes } from 'appConstants';
 import styles from './styles.module.scss';
 import { RequestsType } from './types';
+
+type RequestCellProps = {
+  requester: string;
+};
+
+const RequestCell: React.FC<RequestCellProps> = ({ requester }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <span>{requester}</span>
+      <button
+        className={styles.table_btn_link}
+        onClick={() => setIsOpen((state) => !state)}
+      >
+        See the profile details
+      </button>
+      {isOpen && (
+        <Requester
+          name="John Doe"
+          contry="London, UK (GMT +0)"
+          organization="London Institute of Medical Sciences, Head of neurosurgery laboratory"
+          position="Head of neurosurgery laboratory"
+          fields={'Neurobiology, neurosurgery, neuropathology'.split(', ')}
+          socialMedia="https:/facebook.com/profile"
+          avatar="https://www.figma.com/file/bknHsaOyZlzB3FrosPJ7Vx/ARCHON-(Copy)?type=design&node-id=526-4546&mode=design&t=cjGucjlcUhk4ouS0-4"
+          onCloseModal={() => {
+            setIsOpen(false);
+          }}
+        />
+      )}
+    </>
+  );
+};
 
 export const useColumns = () =>
   useMemo(
@@ -18,11 +51,11 @@ export const useColumns = () =>
             original: { name },
           },
         }: ItemRowProps<RequestsType>) =>
-          (name ? (
+          name ? (
             <Link href={`${routes.storage.root}/${name}`}>{name}</Link>
           ) : (
             '-'
-          )),
+          ),
       },
       {
         Header: 'Request date',
@@ -41,14 +74,7 @@ export const useColumns = () =>
             original: { requester },
           },
         }: ItemRowProps<RequestsType>) =>
-          (requester ? (
-            <>
-              <span>{requester}</span>
-              <Link href="/#">See the profile details</Link>
-            </>
-          ) : (
-            '-'
-          )),
+          requester ? <RequestCell requester={requester} /> : '-',
       },
       {
         Header: 'Access action',
@@ -58,7 +84,7 @@ export const useColumns = () =>
             original: { id },
           },
         }: ItemRowProps<RequestsType>) =>
-          (id ? (
+          id ? (
             <div className={styles.table_block_btn}>
               <Button className={styles.table_provide}>Provide</Button>
               <Button
@@ -70,8 +96,8 @@ export const useColumns = () =>
             </div>
           ) : (
             '-'
-          )),
+          ),
       },
     ],
-    [],
+    []
   );
