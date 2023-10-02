7bc2c513c3e95bbfe94f6763982a6ba752500cd4
IntegralTeam <repo@sfxdx.com>
2023-10-02 14:14:25 +0300

    add Upload page

---
+++ src/containers/Upload/index.tsx
@@ -0,0 +1,91 @@
+import { Typography } from 'components';
+import { useScreenWidth } from 'hooks';
+import { ScreenWidth } from 'appConstants';
+import styles from './styles.module.scss';
+import { DragNDrop } from './DragNDrop';
+import { Item } from './Item';
+
+type DataProps = {
+  name: string;
+  percents: number;
+  weight: number;
+};
+
+const data: DataProps[] = [
+  {
+    name: 'Advancements in Biomedical Research',
+    percents: 80,
+    weight: 513,
+  },
+  {
+    name: 'Precision Medicine',
+    percents: 70,
+    weight: 278,
+  },
+  {
+    name: 'Nanotechnologies in medicine',
+    percents: 100,
+    weight: 100,
+  },
+  {
+    name: 'Nanotechnologies in medicine',
+    percents: 100,
+    weight: 100,
+  },
+];
+
+export const Upload = () => {
+  const isMobile = useScreenWidth(ScreenWidth.mobile);
+  // const [file, setFile] = useState<FileList | null>(null);
+
+  // const onUploadClick = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+  //   const file = e.target.files;
+  //   setFile(file);
+  // }, []);
+
+  return (
+    <div className={styles.upload}>
+      <Typography
+        className={styles.upload_title}
+        type="h3"
+      >
+        Upload activity
+      </Typography>
+
+      <div className={styles.upload_dnd}>
+        <DragNDrop />
+      </div>
+
+      {!isMobile && (
+        <label
+          htmlFor="upload"
+          className={styles.upload_btn}
+        >
+          Select a file from local directory
+          <input
+            type="file"
+            id="upload"
+            // onChange={onUploadClick}
+            className={styles.upload_input}
+          />
+        </label>
+      )}
+
+      <div className={styles.statuses}>
+        <Typography
+          className={styles.statuses_title}
+          type="h4"
+        >
+          Statuses
+        </Typography>
+        {data.map(({ name, percents, weight }) => (
+          <Item
+            name={name}
+            percents={percents}
+            weight={weight}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -5,3 +5,4 @@ export { Login } from './Login';
 export { Registration } from './Registration';
 export { ForgotPassword } from './ForgotPassword';
 export { Storage } from './Storage';
+export { Upload } from './Upload';
