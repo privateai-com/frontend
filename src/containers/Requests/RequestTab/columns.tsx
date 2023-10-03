50e7a6b7e67a864cfe3006ae9ef5bec18e756e27
IntegralTeam <repo@sfxdx.com>
2023-10-03 16:51:39 +0300

    refactor RequestTab

--- src/containers/Requests/RequestTab/columns.tsx
+++ src/containers/Requests/RequestTab/columns.tsx
@@ -1,65 +1,12 @@
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 import Link from 'next/link';
-import { Button, Requester } from 'components';
+import { Button, RequestCell } from 'components';
 import { ItemRowProps } from 'types';
 import { routes } from 'appConstants';
-import { Loader } from 'components/Loader';
-import { useModal } from 'react-modal-hook';
 import { TitleWithArrows } from 'components/AdaptivePaginationTable/TitleWithArrows';
 import styles from './styles.module.scss';
 import { RequestsType } from './types';
-
-type RequestCellProps = {
-  requester: string;
-};
-
-const RequestCell: React.FC<RequestCellProps> = ({ requester }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [showRequester, hideRequester] = useModal(
-    () => (
-      <Requester
-        avatar="https://www.figma.com/file/bknHsaOyZlzB3FrosPJ7Vx/ARCHON-(Copy)?type=design&node-id=526-4546&mode=design&t=cjGucjlcUhk4ouS0-4"
-        name="John Doe"
-        contry="London, UK (GMT +0)"
-        organization="London Institute of Medical Sciences, Head of neurosurgery laboratory"
-        position="Head of neurosurgery laboratory"
-        fields={'Neurobiology, neurosurgery, neuropathology'.split(', ')}
-        socialMedia="https:/facebook.com/profile"
-        onCloseModal={() => {
-          hideRequester();
-        }}
-      />
-    ),
-    [],
-  );
-
-  const onHandlerClick = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      showRequester();
-    }, 1500);
-  };
-
-  return (
-    <>
-      <span>{requester}</span>
-      <div className={styles.table_block_btn}>
-        {!isLoading ? (
-          <button
-            className={styles.table_btn_link}
-            onClick={onHandlerClick}
-          >
-            See the profile details
-          </button>
-        ) : (
-          <Loader />
-        )}
-      </div>
-    </>
-  );
-};
 
 export const useColumns = () =>
   useMemo(
@@ -95,7 +42,13 @@ export const useColumns = () =>
             original: { requester },
           },
         }: ItemRowProps<RequestsType>) =>
-          (requester ? <RequestCell requester={requester} /> : '-'),
+          (requester ? (
+            <RequestCell requester={requester}>
+              See the profile details
+            </RequestCell>
+          ) : (
+            '-'
+          )),
       },
       {
         Header: 'Access action',
