74346e5bb36d20581ef1a22a0ce52ac308c495b9
IntegralTeam <repo@sfxdx.com>
2023-10-03 16:47:04 +0300

    add CommonTab

---
+++ src/containers/Storage/CommonTab/index.tsx
@@ -0,0 +1,56 @@
+import { ReactNode, useState } from 'react';
+
+import { AdaptivePaginationTable, TextInput } from 'components';
+
+import { Content } from 'types';
+import styles from './styles.module.scss';
+
+type CommonTableProps = {
+  columns: object[];
+  content: Content[];
+  mobileTitle1: string;
+  key1: string;
+  mobileTitle2: string;
+  key2: string;
+  children?: ReactNode;
+  withPagination?: boolean;
+};
+
+const CommmonTab: React.FC<CommonTableProps> = ({
+  columns,
+  content,
+  mobileTitle1,
+  key1,
+  mobileTitle2,
+  key2,
+  children,
+  withPagination,
+}) => {
+  const [search, setSearch] = useState('');
+
+  return (
+    <>
+      <div className={styles.storage__search}>
+        <TextInput
+          value={search}
+          onChangeValue={setSearch}
+          placeholder="Search by file name"
+          isSearch
+          classNameInputBox={styles.input}
+        />
+        {children}
+      </div>
+      <AdaptivePaginationTable
+        columns={columns}
+        content={content}
+        mobileTitle1={mobileTitle1}
+        key1={key1}
+        mobileTitle2={mobileTitle2}
+        key2={key2}
+        withPagination={withPagination}
+      />
+    </>
+  );
+};
+
+export { CommmonTab };
