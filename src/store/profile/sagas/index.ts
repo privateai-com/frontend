e0ab4a3e4f582547c20c8ec85d37c65e1469a8d9
IntegralTeam <repo@sfxdx.com>
2023-10-09 12:39:51 +0300

    rename to profile saga

--- src/store/profile/sagas/index.ts
+++ src/store/profile/sagas/index.ts
@@ -2,6 +2,6 @@ import { takeLatest } from 'redux-saga/effects';
 import { ProfileActionTypes } from '../actionTypes';
 import { profileLinkWalletSaga } from './linkWallet';
 
-export default function* authSaga() {
+export default function* profileSaga() {
   yield takeLatest(ProfileActionTypes.LinkWallet, profileLinkWalletSaga);
 }
