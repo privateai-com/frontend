ee671eab1e7ea530600da7221e9a111ae2d3865b
IntegralTeam <repo@sfxdx.com>
2023-10-11 16:14:48 +0300

    fix height

--- src/containers/KnowledgeBase/index.tsx
+++ src/containers/KnowledgeBase/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import { TextInput, Typography } from 'components';
+import { Typography } from 'components';
 import styles from './styles.module.scss';
 import { Item } from './Item';
 import { items } from './data';
 
 export const KnowledgeBase: React.FC = () => {
-  const [search, setSearch] = useState('');
+  const search = 'gene';
   return (
     <div className={styles.knowledge}>
       <div className={styles.knowledge_header}>
@@ -15,35 +15,31 @@ export const KnowledgeBase: React.FC = () => {
         >
           Knowledge base
         </Typography>
-        <TextInput
-          classNameContainer={styles.knowledge_search}
-          value={search}
-          onChangeValue={setSearch}
-          placeholder="Search"
-          isSearch
-        />
       </div>
       <div className={styles.items}>
-        <div className={styles.items_container}>
-          {items.map(
-            ({
-              name, author, field, core, status, created, modified, 
-            }, ind) => (
-              <Item
-                key={ind} // eslint-disable-line react/no-array-index-key
-                name={name}
-                field={field}
-                author={author}
-                core={core}
-                status={status}
-                created={created}
-                modified={modified}
-                search={search}
-              />
-            ),
-          )}
+        <div className={styles.items_wrapper}>
+          <div className={styles.items_container}>
+            {items.map(
+              (
+                { name, author, field, core, status, created, modified },
+                ind
+              ) => (
+                <Item
+                  key={ind} // eslint-disable-line react/no-array-index-key
+                  name={name}
+                  field={field}
+                  author={author}
+                  core={core}
+                  status={status}
+                  created={created}
+                  modified={modified}
+                  search={search}
+                />
+              )
+            )}
+          </div>
         </div>
-      </div>
+      </div>{' '}
     </div>
   );
 };
