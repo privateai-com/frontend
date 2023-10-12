401ffb4cbf435cd1492cf2a47e47113ab38fdb57
IntegralTeam <repo@sfxdx.com>
2023-10-12 14:16:52 +0300

    add deleteWalletSaga

--- src/store/profile/sagas/index.ts
+++ src/store/profile/sagas/index.ts
@@ -1,7 +1,9 @@
 import { takeLatest } from 'redux-saga/effects';
 import { ProfileActionTypes } from '../actionTypes';
 import { profileLinkWalletSaga } from './linkWallet';
+import { profileDeleteWalletSaga } from './profileDeleteWalletSaga';
 
 export default function* authSaga() {
   yield takeLatest(ProfileActionTypes.LinkWallet, profileLinkWalletSaga);
+  yield takeLatest(ProfileActionTypes.DeleteWallet, profileDeleteWalletSaga);
 }
---
+++ src/store/profile/sagas/profileDeleteWalletSaga.ts
@@ -0,0 +1,31 @@
+import { call, put } from 'redux-saga/effects';
+
+import { sagaExceptionHandler } from 'utils';
+import { AccountState, RequestStatus } from 'types';
+import { ApiEndpoint, callApi } from 'appConstants';
+import { accountSetState } from 'store/account/actionCreators';
+import { profileDeleteWallet, profileSetStatus } from '../actionCreators';
+
+export function* profileDeleteWalletSaga({
+  type,
+}: ReturnType<typeof profileDeleteWallet>) {
+  try {
+    yield put(profileSetStatus({ type, status: RequestStatus.REQUEST }));
+
+    yield call(callApi, {
+      method: 'DELETE',
+      endpoint: ApiEndpoint.ProfileDeleteWallet,
+    });
+
+    yield put(
+      accountSetState({
+        walletAddress: null,
+      })
+    );
+
+    yield put(profileSetStatus({ type, status: RequestStatus.SUCCESS }));
+  } catch (e) {
+    sagaExceptionHandler(e);
+    yield put(profileSetStatus({ type, status: RequestStatus.ERROR }));
+  }
+}
