e7c49aa2768d23b6c77788395fc1c357c0f1025b
IntegralTeam <repo@sfxdx.com>
2023-10-16 14:12:03 +0300

    add scroll to notification

--- src/containers/Header/Notification/index.tsx
+++ src/containers/Header/Notification/index.tsx
@@ -24,34 +24,36 @@ const items = [
 
 const Notification = memo(({ isOpen }: NotificationProps) => (
   <div className={cx(styles.notification_container, { [styles.show]: isOpen })}>
-    {items.map(({ text, time }) => (
-      <div
-        className={styles.item_container}
-        key={text}
-      >
-        <div className={styles.item_block}>
-          <div className={styles.item_time}>{time}</div>
-          <ButtonIcon
-            className={styles.item_close}
-            onClick={() => {}}
-            image={closeModalIcon}
-            height={12}
-            width={12}
-          />
-        </div>
-        <Link
-          href={`${routes.requests.root}`}
-          className={styles.item_content}
+    <div className={styles.notification_content}>
+      {items.map(({ text, time }) => (
+        <div
+          className={styles.item_container}
+          key={text}
         >
-          {text}
-          <Image
-            src={arrowIcon}
-            alt={text}
-            width={25}
-          />
-        </Link>
-      </div>
-    ))}
+          <div className={styles.item_block}>
+            <div className={styles.item_time}>{time}</div>
+            <ButtonIcon
+              className={styles.item_close}
+              onClick={() => {}}
+              image={closeModalIcon}
+              height={14}
+              width={14}
+            />
+          </div>
+          <Link
+            href={`${routes.requests.root}`}
+            className={styles.item_content}
+          >
+            {text}
+            <Image
+              src={arrowIcon}
+              alt={text}
+              width={25}
+            />
+          </Link>
+        </div>
+      ))}
+    </div>
   </div>
 ));
 
