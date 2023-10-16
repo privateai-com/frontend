73cd681da62a24d01c9502e3cd760a5e64d4f00e
IntegralTeam <repo@sfxdx.com>
2023-10-16 16:49:14 +0300

    add getStatusStyle n getStatusImg

---
+++ src/containers/Storage/RequestedTab/utils.ts
@@ -0,0 +1,44 @@
+import { arrowDownSquare, tryIcon, xSquareIcon } from 'assets';
+
+export const getStatusStyle = (
+  status: string,
+  styles: {
+    readonly [key: string]: string;
+  }
+) => {
+  switch (status) {
+    case 'Open sourced':
+      return styles.open;
+
+    case 'Access granted':
+      return styles.granted;
+
+    case 'Permission needed':
+      return styles.permission_needed;
+
+    case 'Access request pending':
+      return styles.pending;
+
+    case 'Access denied':
+      return styles.denied;
+
+    default:
+      return styles.item_status;
+  }
+};
+
+export const getStatusImg = (status: string) => {
+  switch (status) {
+    case 'Access granted':
+      return arrowDownSquare;
+
+    case 'Access request pending':
+      return xSquareIcon;
+
+    case 'Access denied':
+      return tryIcon;
+
+    default:
+      return arrowDownSquare;
+  }
+};
