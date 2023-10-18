852a148baecaa31a8a1cd1e65fe4fb813a200aa5
IntegralTeam <repo@sfxdx.com>
2023-10-18 15:35:40 +0300

    delete icon

--- src/assets/img/icons/index.ts
+++ src/assets/img/icons/index.ts
@@ -37,7 +37,6 @@ export { default as arrowDownSquare } from './arrowDownSquare.svg';
 export { default as logoIcon } from './logo.svg';
 export { default as helpCircleIcon } from './helpCircle.svg';
 export { default as xSquareIcon } from './xSquare.svg';
-export { default as tryIcon } from './try.svg';
 
 export { default as bg1 } from './bg1.svg';
 export { default as bg2 } from './bg2.svg';
