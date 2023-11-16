da4a4b30df1a907fbc53f2544cbc8f4247f893ba
IntegralTeam <repo@sfxdx.com>
2023-11-16 10:26:11 +0000

    Task/#90 fix security page

--- src/containers/Security/index.tsx
+++ src/containers/Security/index.tsx
@@ -14,6 +14,12 @@ import { useScreenWidth } from 'hooks';
 import { ScreenWidth } from 'appConstants';
 import styles from './styles.module.scss';
 
+const anchorId = {
+  keyPassword: 'key-password',
+  vault: 'vault',
+  dataSecureAndPrivate: 'data-secure-and-private',
+};
+
 const Security = () => {
   const isMobile = useScreenWidth(ScreenWidth.mobile);
   const isTablet = useScreenWidth(ScreenWidth.notebook1024);
@@ -50,64 +56,69 @@ const Security = () => {
               Summary/introduction
             </Typography>
             <div className={styles.main_text}>
-              Ensuring the security of user data is of paramount importance for
-              the Archon team. In today&apos;s digital landscape, where data
-              breaches and unauthorized access are prevalent, implementing
-              robust security mechanisms is crucial. Archon employs a
-              multi-layered approach to safeguard user data, with a focus on
-              three main mechanisms:
+              Ensuring the security of user data is of paramount importance for 
+              the PrivateAI team. In today&apos;s digital landscape, where data 
+              breaches and unauthorized access are prevalent, implementing 
+              robust security mechanisms is crucial. PrivateAI employs a 
+              multi-layered approach to safeguarding user data, with a focus on 
+              three main mechanisms: 
               <ul>
                 <br />
                 <li>
                   <strong>RSA </strong>
-                  asymmetric encryption protocol - a well-known and time-tested
-                  protocol that permits to encrypt all the files stored within
-                  the Archon system and provide access to it only to its owner
-                  and authorized users. When a user uploads a file to the Archon
-                  system, it undergoes encryption using RSA. This means that
-                  even if an unauthorized party gains access to the encrypted
-                  files, they would be unable to decipher the content. (RSA)
+                  asymmetric encryption protocol - a well-known and time-tested protocol
+                  that facilitates the encryption of all the files stored within the PrivateAI
+                  system and provides access to it only to its owner and authorized users.
                   {' '}
                 </li>
                 <li>
                   <strong>key password</strong>
                   {' '}
-                  - an additional layer of
-                  security implying the encryption of the owner’s private key
-                  with a custom password. It allows storing the users’
-                  encryption keys on the backend side of the Archon application
-                  without lowering the security level. (Key Password)
+                  - an additional layer of security implying the encryption 
+                  of the owner’s private key with a custom password. 
+                  It allows storing the users’ encryption keys on the backend 
+                  side of the PrivateAI application without 
+                  lowering the security level. 
+                  {' '}
+                  (
+                  <a href={`#${anchorId.keyPassword}`}> Key Password</a>
+                  )
                 </li>
                 <li>
                   <strong>a backend vault for key storage</strong>
                   {' '}
-                  - a special
-                  backend zone with additional security techniques for storing
-                  sensitive information, the users’ encryption keys in
-                  particular. (Vault)
+                  a special backend zone with additional security techniques
+                  for storing sensitive information, the users’
+                  encryption keys in particular.
+                  {' '}
+                  (
+                  <a href={`#${anchorId.vault}`}> Vault</a>
+                  )
                 </li>
                 <br />
               </ul>
-              By combining these measures, Archon aims to provide users with a
-              secure and trustworthy platform for their data storage. In the
-              following sections, we will delve into the details of each used
-              mechanism, highlighting their significance and how they contribute
-              to the overall security framework of Archon.
-              {' '}
+              By combining these measures, PrivateAI aims to provide users with a 
+              secure and trustworthy platform for their data storage. 
+              In the following sections, we will delve into the details of 
+              each mechanism, highlighting their significance and 
+              how they contribute to the overall security.
+              <br />
+
             </div>
             <Typography
               className={styles.main_title}
               type="h3"
-              id="link1"
+              id={anchorId.dataSecureAndPrivate}
             >
               How do we keep your data secure and private?
             </Typography>
             <div className={styles.main_text}>
-              The Archon team considers the security of users’ data as the
-              highest priority and tends to increase the security level using
-              all possible opportunities. In this paragraph we briefly tell you
-              about the way your file is encrypted and how it helps to safeguard
-              the file from unauthorized access.
+              The PrivateAI team considers the security of users’ 
+              data as the highest priority and tends to increase the 
+              security level using all possible opportunities. 
+              In this paragraph we briefly tell you about the way your file 
+              is encrypted and how it helps to safeguard the file from unauthorized access.
+
               <br />
               <br />
               {' '}
@@ -125,11 +136,10 @@ const Security = () => {
               {' '}
               <strong>encryption</strong>
               {' '}
-              is the process that converts the
-              original representation of the information, known as plaintext,
-              into an alternative form known as ciphertext. The goal is to let
-              only authorized parties decrypt the ciphertext and get access to
-              the valuable original information.
+              is the process that converts the original representation of 
+              the information, known as plaintext, into an alternative form 
+              known as ciphertext. The goal is to let only authorized 
+              parties decrypt the ciphertext and get access to the valuable original information.
               <br /> 
               {' '}
               <br />
@@ -139,14 +149,13 @@ const Security = () => {
               <br />
               <strong> Symmetric protocols</strong>
               {' '}
-              generate one key that allows
-              both encrypt and decrypt the information, i.e. sender and
-              recipient use the same key. It is presented on the schemes below.
-              In case Bob wants to transfer a file to Alice, they should
-              beforehand somehow agree on which encryption key to use and keep
-              it in secret. After the agreement is achieved, both parties can
-              encrypt and decrypt messages for and from the other party using
-              the same key.
+              generate one key that allows both encrypt and decrypt the 
+              information, i.e. sender and recipient use the same key. 
+              It is presented on the schemes below. In case Bob wants to 
+              transfer a file to Alice, they should beforehand somehow agree on 
+              which encryption key to use and keep it in secret. 
+              After the agreement is achieved, both parties can encrypt and 
+              decrypt messages for and from the other party using the same key.
             </div>
             <div className={styles.main_block_halfed}>
               <div className={styles.main_text}>
@@ -156,20 +165,18 @@ const Security = () => {
                   <li>
                     <strong>Key Distribution</strong>
                     {' '}
-                    - both should securely
-                    distribute the encryption key before they can use the
-                    system. If the key falls into the malicious party during
-                    distribution, it compromises the security of the entire
-                    system.
+                    - both should securely distribute the encryption 
+                    key before they can use the system. 
+                    If the key falls into the malicious party during distribution, 
+                    it compromises the security of the entire system.
                   </li>
                   <li>
                     <strong>Scalability</strong>
                     {' '}
-                    - symmetric encryption becomes
-                    more challenging to manage as the number of parties involved
-                    increases. With each additional participant, a unique key
-                    needs to be securely distributed and that could be a complex
-                    task when.
+                    - symmetric encryption becomes more challenging to manage as 
+                    the number of parties involved increases. 
+                    With each additional participant, a unique key needs to be securely 
+                    distributed and that could be a complex task when .
                   </li>
                 </ul>
                 More information about the symmetric encryption can be found
@@ -203,24 +210,25 @@ const Security = () => {
               </div>
             </div>
             <div className={styles.main_text}>
-              Unlike the symmetric protocols, the asymmetric type of encryption
-              does not involve the aforementioned problems and therefore it is a
-              better solution for many applications.
+              Unlike the symmetric protocols, the asymmetric type of encryption does 
+              not involve the aforementioned problems and therefore it is 
+              a better solution for many applications.
               <br />
               <br />
               <strong>Asymmetric encryption mechanism</strong>
               {' '}
-              implies the usage
-              of a key pair by each user - the public and the private one. The
-              public key as it can be seen from its name can be shared with any
-              other party without compromising any data, whereas the private key
-              should be kept in secret by its owner. The public key is used for
-              the encrypting of the information, so one can always take any
-              public key and encrypt data for the corresponding user.
+              implies the usage of a key pair by each user - 
+              the public and the private one. 
+              The public key as it can be seen from its name can be shared 
+              with any other party without compromising any data, 
+              whereas the private key should be kept in secret by its owner. 
+              The public key is used for the encrypting of the information, 
+              so one can always take any public key and encrypt data for 
+              the corresponding user.
               <br />
-              However, since the private key is needed for decrypting the data,
-              only the intended recipient who possesses the corresponding
-              private key can decrypt and access the information.
+              However, since the private key is needed for 
+              decrypting the data, only the intended recipient who possesses 
+              the corresponding private key can decrypt and access the information.
             </div>
             <div className={styles.main_block_halfed}>
               <div className={styles.main_text}>
@@ -390,32 +398,33 @@ const Security = () => {
                 {' '}
                 <br />
                 {' '}
-                Apart from the mentioned methods, the Archon
-                system incorporates a feature known as multikey encryption. This
-                serves as a complement to asymmetric protocols and enables the
-                encryption of a file with multiple public keys. Consequently,
-                the file can be decrypted using any corresponding private key.
-                This mechanism offers flexibility in granting access to
-                encrypted data to multiple recipients, which is particularly
-                useful when a file needs to be shared with numerous users. With
-                multikey encryption, there is no need to individually encrypt
-                the data with different users&apos; private keys and store
-                multiple copies. Instead, only one encrypted file copy needs to
-                be stored, accessible to all authorized parties.
+                Apart from the mentioned methods, the PrivateAI 
+                system incorporates a feature known as multikey encryption. 
+                This serves as a complement to asymmetric protocols and enables 
+                the encryption of a file with multiple public keys. 
+                Consequently, the file can be decrypted using any 
+                corresponding private key. This mechanism offers flexibility in 
+                granting access to encrypted data to multiple recipients, 
+                which is particularly useful when a file needs to be shared 
+                with numerous users. With multikey encryption, there is no need 
+                to individually encrypt the data with different users&apos; 
+                private keys and store multiple copies. 
+                Instead, only one encrypted file copy needs to be stored, 
+                accessible to all authorized parties.
                 <br /> 
                 {' '}
                 <br />
                 {' '}
-                By combining these principles, the Archon creates
-                a multi-layered security approach that maximizes the protection
-                of its storages. This comprehensive strategy ensures that
-                sensitive information and valuable assets remain secure.
+                By combining these principles, the PrivateAI creates a multi-layered
+                security approach that maximizes the protection of its storages.
+                This comprehensive strategy ensures that sensitive information
+                and valuable assets remain secure.
               </div>
             </div>
             <Typography
               className={styles.main_title}
               type="h3"
-              id="link2"
+              id={anchorId.keyPassword}
             >
               What is the key password and why is it needed?
             </Typography>
@@ -505,26 +514,27 @@ const Security = () => {
             <Typography
               className={styles.main_title}
               type="h3"
+              id={anchorId.vault}
             >
               Vault
             </Typography>
             <div className={styles.main_text}>
-              The Archon project integrates a popular open-source tool called
+              The PrivateAI project integrates a popular open-source tool called
               HashiCorp Vault which is commonly used for securely storing and
               accessing sensitive data in modern computing environments.
               <br />
               <br />
               Vault provides a centralized solution for managing so-called
               secrets or in other words sensitive information such as passwords,
-              API tokens or database credentials. As for the Archon main use
+              API tokens or database credentials. As for the PrivateAI main use
               case in particular, it is the secure storing and monitoring of the
               users’ encryption keys whose concepts and usage were described in
               previous sections (
               <span>
-                <a href="#link1">link1</a>
+                <a href={`#${anchorId.dataSecureAndPrivate}`}> How do we keep your data secure and private?</a>
                 {' '}
                 and
-                <a href="#link2"> link2</a>
+                <a href={`#${anchorId.keyPassword}`}> What is the key password and why is it needed?</a>
                 ).
               </span>
               <br /> 
@@ -551,7 +561,7 @@ const Security = () => {
             </div>
             <div className={styles.main_text}>
               Vault provides wide opportunities for further development of the
-              Archon project and upgrading its architecture since it supports
+              PrivateAI project and upgrading its architecture since it supports
               various popular services, for example, authentication methods,
               including tokens, username/password, and multi-factor
               authentication. It integrates with popular identity providers like
@@ -575,7 +585,7 @@ const Security = () => {
               Overall, HashiCorp Vault is a powerful tool for
               managing secrets securely, providing organizations with a robust
               solution to protect sensitive data in modern computing
-              environments. Archon integrates it for secure storing of the
+              environments. PrivateAI integrates it for secure storing of the
               users&apos; encryption key at the current stage of the project
               development as a strong and reliable tool, but it also opens huge
               possibilities for further security system upgrade, complication
@@ -585,10 +595,6 @@ const Security = () => {
               <br />
               {' '}
               <span className={styles.main_last_words}>
-                <br /> 
-                {' '}
-                <br />
-                {' '}
                 These principles and much more are described in
                 detail in the documentation of HashiCorp itself and you can see
                 it in case you are interested in more specific nuances -
