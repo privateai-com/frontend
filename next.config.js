8adf1c642c1ffde22822a77dd8a9040a33012724
IntegralTeam <repo@sfxdx.com>
2023-12-13 16:26:10 +0700

    add dev images

--- next.config.js
+++ next.config.js
@@ -33,6 +33,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'archon.sfxdx.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
