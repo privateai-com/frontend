4a0e05ec7ca01483cb32526709a6dc68082bc4b9
IntegralTeam <repo@sfxdx.com>
2023-12-13 15:39:24 +0700

    update images nextjs

--- next.config.js
+++ next.config.js
@@ -21,6 +21,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'frontend-demo.privateai.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
