dd4869389f269043f6f53385b8ae4252b89e0761
IntegralTeam <repo@sfxdx.com>
2023-12-19 14:19:33 +0700

    edit message metamask

--- src/appConstants/errorsNotification.ts
+++ src/appConstants/errorsNotification.ts
@@ -2,7 +2,9 @@ export const errorsNotification = {
   metamaskError:
     'It seems like you do not have a wallet extension in your browser. Please install it and try one more time.',
   walletNotAttachedError:
-    'You are trying to connect with a wallet that is not attached to any account. Please connect another wallet or sign in using your credentials.',
+    // 'You are trying to connect with a wallet that is not attached to any account.
+    // Please connect another wallet or sign in using your credentials.',
+    "Looks like you're attempting to connect with a wallet that isn't linked to any account.Please try connecting another wallet or sign in using your credentials to proceed.",
   existEmailError:
     'An account with entered email address already exists. Please enter another email or sign in.',
   authError:
