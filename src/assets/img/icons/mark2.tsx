f0fd1dda86e8bc96c06010c31a3daa9ac74c954f
NikShamin <o-l-y-m-p-i-c@mail.ru>
2024-02-21 17:13:30 +0300

    mark added

---
+++ src/assets/img/icons/mark2.tsx
@@ -0,0 +1,22 @@
+
+
+export const Mark2 = () => {
+    return  <svg width="18" height="18" viewBox="0 0 49 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="17" y="11" width="14" height="28" fill="black"/>
+    <g filter="url(#filter0_i_1728_437)">
+    <path d="M5.719 44.414H42.539C46.055 44.414 48.258 41.883 48.258 38.695C48.2621 37.7077 48.0032 36.7371 47.508 35.883L29.063 2.883C28.008 0.983999 26.086 0 24.141 0C22.219 0 20.251 0.983999 19.195 2.883L0.774 35.906C0.258 36.796 0 37.734 0 38.696C0 41.883 2.227 44.414 5.719 44.414ZM24.141 28.359C22.899 28.359 22.219 27.656 22.172 26.391L21.844 14.813C21.797 13.547 22.781 12.609 24.117 12.609C25.43 12.609 26.461 13.57 26.414 14.836L26.063 26.391C26.016 27.68 25.336 28.359 24.141 28.359ZM24.141 37.008C22.781 37.008 21.516 35.93 21.516 34.476C21.516 33.023 22.758 31.946 24.141 31.946C25.524 31.946 26.766 33 26.766 34.476C26.766 35.953 25.5 37.008 24.141 37.008Z" fill="#FFD600"/>
+    </g>
+    <defs>
+    <filter id="filter0_i_1728_437" x="0" y="0" width="48.2581" height="44.4141" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+    <feOffset/>
+    <feGaussianBlur stdDeviation="5"/>
+    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 0.705882 0 0 0 0 0.00392157 0 0 0 1 0"/>
+    <feBlend mode="normal" in2="shape" result="effect1_innerShadow_1728_437"/>
+    </filter>
+    </defs>
+    </svg>
+}
--- src/components/LikeBtnWrapper/index.tsx
+++ src/components/LikeBtnWrapper/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import cx from 'classnames';
 import { CommunityButton } from 'components/CommunityButton';
 import { isArticlePopular } from 'utils';
 // import { LikeFilledIcon, LikeIcon } from 'assets';
 import LikeIcon2 from 'assets/img/icons/LikeIcon2';
 import styles from './styles.module.scss';
+import { Mark2 } from 'assets/img/icons/mark2';
 
 interface CommunityButtonPropsWrap {
   props: {
@@ -38,6 +39,8 @@ export const LikeBtnWrapper: React.FC<CommunityButtonPropsWrap> = (
 
   const [clicked, setClicked] = useState(false);
 
+  const [isTooltipOpen, setTooltipFlag] = useState(false);
+
   const handleOnClick = () => {
     onLike();
     // if(!liked){
@@ -47,62 +50,107 @@ export const LikeBtnWrapper: React.FC<CommunityButtonPropsWrap> = (
     setClicked(!liked);
   };
 
+  const dropdownRef = useRef(null)
+  
+
+  useEffect(()=>{
+   document.addEventListener("click", ()=>{
+    // setTooltipFlag(false)
+    if (dropdownRef && dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setTooltipFlag(false);
+    }
+   }) 
+  })
+
+
   return (
-    <div className={styles.btnWrap}>
-      <CommunityButton
-        className={
-          cx(
-            [styles.likeBtn, styles.btn, liked && 
-            styles.active, isDisabled && styles.disabled]
-          )
-        }
-        isLiked={liked}
-        onClick={handleOnClick}
-        count={likesCount}
-        isDisabled={isDisabled}
-        _custom
-        _defaultIcon={(
-          <div className={cx([styles.iconWrap, liked && styles.active, clicked && styles.play])}>
+    <div className={styles.btnWrapOuter}>
+      <div className={styles.btnWrap}>
+        <CommunityButton
+          className={
+            cx(
+              [styles.likeBtn, styles.btn, liked && 
+              styles.active, isDisabled && styles.disabled]
+            )
+          }
+          isLiked={liked}
+          onClick={handleOnClick}
+          count={likesCount}
+          isDisabled={isDisabled}
+          _custom
+          _defaultIcon={(
+            <div className={cx([styles.iconWrap, liked && styles.active, clicked && styles.play])}>
+              <LikeIcon2 
+                handColor={liked ? '#4659FE' : 'transparent'} 
+                borderColor={liked ? '#4659FE' : '#747474'}
+              />
+            </div>
+  )}
+          // _activeIcon={<>
+          //     <LikeIcon2 handColor='green' strokeColor='#747474' />
+          //     {/* <LikeFilledIcon className={styles.filledLike} /> */}
+          // </>}
+          // isDisabled={
+          //   ![StatusAccessArticle.OpenSource, StatusAccessArticle.AccessGranted]
+          //     .includes(status as StatusAccessArticle) ||
+          //   isDisabled
+          // }
+          isPopular={isArticlePopular(likesCount, dislikesCount)}
+        />
+        <span className={cx([styles.divider, isDisabled && styles.disabled])} />
+        <CommunityButton
+          className={cx([styles.dislikeBtn, styles.btn, disliked && styles.active])}
+          isDisliked={disliked}
+          onClick={onDislike}
+          count={dislikesCount}
+          isDisabled={isDisabled}
+          _custom
+          _defaultIcon={(
             <LikeIcon2 
-              handColor={liked ? '#4659FE' : 'transparent'} 
-              borderColor={liked ? '#4659FE' : '#747474'}
+              handColor={disliked ? '#747474' : 'transparent'} 
+              borderColor="#747474"
+              strokeColor={!disliked ? '#747474' : '#F3F5FF'}
             />
+  )}
+          isDislikeButton
+          // isDisabled={
+          //   ![StatusAccessArticle.OpenSource, StatusAccessArticle.AccessGranted]
+          //     .includes(status as StatusAccessArticle) ||
+          //   isDisabled
+          // }
+          isPopular={isArticlePopular(dislikesCount, likesCount)}
+        />
+        
+      </div>
+      {
+        isArticlePopular(dislikesCount, likesCount) && 
+        <div className="" ref={dropdownRef}>
+          <div className={styles.tooltip_btn} 
+            onClick={()=>{
+              setTooltipFlag(prev => !prev)
+            }} 
+            // onMouseOver={()=>{
+            //   setTooltipFlag(true)
+            // }} 
+            // onMouseLeave={()=>{
+            //   setTooltipFlag(false)
+            // }}
+            // onMouseOut={()=>{
+            //   setTooltipFlag(false)
+            // }}
+          >
+           <Mark2 />
           </div>
-)}
-        // _activeIcon={<>
-        //     <LikeIcon2 handColor='green' strokeColor='#747474' />
-        //     {/* <LikeFilledIcon className={styles.filledLike} /> */}
-        // </>}
-        // isDisabled={
-        //   ![StatusAccessArticle.OpenSource, StatusAccessArticle.AccessGranted]
-        //     .includes(status as StatusAccessArticle) ||
-        //   isDisabled
-        // }
-        isPopular={isArticlePopular(likesCount, dislikesCount)}
-      />
-      <span className={cx([styles.divider, isDisabled && styles.disabled])} />
-      <CommunityButton
-        className={cx([styles.dislikeBtn, styles.btn, disliked && styles.active])}
-        isDisliked={disliked}
-        onClick={onDislike}
-        count={dislikesCount}
-        isDisabled={isDisabled}
-        _custom
-        _defaultIcon={(
-          <LikeIcon2 
-            handColor={disliked ? '#747474' : 'transparent'} 
-            borderColor="#747474"
-            strokeColor={!disliked ? '#747474' : '#F3F5FF'}
-          />
-)}
-        isDislikeButton
-        // isDisabled={
-        //   ![StatusAccessArticle.OpenSource, StatusAccessArticle.AccessGranted]
-        //     .includes(status as StatusAccessArticle) ||
-        //   isDisabled
-        // }
-        isPopular={isArticlePopular(dislikesCount, likesCount)}
-      />
+          {isTooltipOpen &&
+          <div className={styles.tooltip} onMouseOver={()=>{
+            setTooltipFlag(true)
+          }} onMouseOut={()=>{
+            setTooltipFlag(false)
+          }}>
+          This article was rated very low by the community. Please proceed with caution.
+          </div>}
+        </div>
+      }
     </div>
   );
 };
