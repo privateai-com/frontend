293bbfd9482c6ba12dcd4b871855f783b5109014
NikShamin <o-l-y-m-p-i-c@mail.ru>
2024-03-21 16:35:33 +0300

    Update texts

--- src/containers/ProfileApiKey/index.tsx
+++ src/containers/ProfileApiKey/index.tsx
@@ -127,12 +127,10 @@ export const ProfileApiKey = () => {
           <div className={styles.api__container_col}>
             <div className={styles.api__container_colored_box}>
               <h3 className={styles.api__container_colored_box_h3} >
-                Create new API key
+                Generate Your API Key
               </h3>
               <p>
-                To use the PrivateAI public API, you should generate your personal API key.
-                You will be required to provide it each time you use API for managing
-                your data or configuring your profile.
+                To access the PrivateAI Public API, you must first create your personal API key. This key is essential and will need to be provided every time you manage your data or configure your profile using the API.
               </p>
               {!apiKey && <div className={styles.profile__buttons_wrap}>
                 {/* <Button onClick={onClickCopy} disabled={!apiKey}>Copy</Button> */}
@@ -156,16 +154,13 @@ export const ProfileApiKey = () => {
                 <path id="*" d="M2.646 6.85294L0 5.92894L0.714 4.08094L3.276 4.96294C4.788 5.46694 5.712 5.92894 6.636 6.51694C6.384 5.46694 6.258 4.41694 6.258 2.90494V0.0909424H8.232V2.90494C8.232 4.41694 8.064 5.50894 7.854 6.51694C8.778 5.97094 9.66 5.46694 11.13 4.96294L13.692 4.03894L14.49 5.92894L11.802 6.85294C10.374 7.35694 9.24 7.60894 8.274 7.73494C9.114 8.49094 9.702 9.20494 10.626 10.5069L12.18 12.7749L10.542 13.9089L8.946 11.6829C8.064 10.4229 7.56 9.41494 7.182 8.49094C6.888 9.45694 6.426 10.3809 5.502 11.6829L3.864 13.9089L2.226 12.7749L3.78 10.5069C4.662 9.20494 5.418 8.44894 6.174 7.73494C4.914 7.56694 3.864 7.31494 2.646 6.85294Z" fill="#7C859E" fillOpacity="0.5" />
               </svg>
               <p>
-                Please do not share this key with anyone to keep the privacy of your account consistent.
-                if you suspect the leakage of your API key, you`d better generate a new one on this page.
+                It is crucial to keep your API key confidential to maintain your account's privacy. If you believe your API key has been compromised, it is strongly advised to revoke  it and generate a new one on this page immediately.
                 <br />
                 <br />
-                Please pay attention that ony one API key is permitted for a single PrivateAI account.
-                Also, note that the throughout is limited to
-                30 request per minute to minimize the server load.
+                Please be aware that only one API key is allowed per PrivateAI account. Additionally, please note that the rate limit is set to 30 requests per minute to reduce server load.
                 <br />
                 <br />
-                You can see the list of available API endpoint and their description here
+                For a list of available API endpoints and their descriptions, please refer to this
                 {' '}
                 <Link href={`${apiBaseUrl}/swagger/api`}>Swagger Link</Link>
               </p>
